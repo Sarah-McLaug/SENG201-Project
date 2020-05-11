2956fd72ae18ba17dd709fa458d0100dff01a3ba
daaanstraight <daaanstraight@gmail.com>
2020-05-11 16:51:00 +1200

    Added Classes: Crop, Animal, Item, CropItem, FoodItem, Game

---
+++ SENG-201-Project/src/item/Item.java
@@ -0,0 +1,25 @@
+package item;
+
+public class Item {
+	private String name;
+	private double price;
+	
+	public Item(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	//BEGIN getters and setters
+	public String getName() {
+		return this.name;
+	}
+	
+	public double getPrice() {
+		return this.price;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	//END getters and setters
+}
