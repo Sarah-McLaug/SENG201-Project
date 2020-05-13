f344614390822c085b3f661f846742f94780ee6f
sarahmclaughlin <smartymcsmartle@gmail.com>
2020-05-13 00:32:39 -0400

    put everything into one main package, added functionality to farm class, moved stuff from different classes and added some basic functionality to crop and animal classes

---
+++ SENG-201-Project/src/main/Crop.java
@@ -0,0 +1,63 @@
+package main;
+
+public class Crop {
+	private String species;
+	private double purchasePrice;
+	private double sellingPrice;
+	private int daysUntilMature;
+	private double health;
+	
+	public Crop(String species, double purchasePrice, double sellingPrice, int daysUntilMature, double health) {
+		this.species = species;
+		this.purchasePrice = purchasePrice;
+		this.sellingPrice = sellingPrice;
+		this.daysUntilMature = daysUntilMature;
+		this.health = health;
+	}
+	
+	//BEGIN getters and setters
+	public String getSpecies() {
+		return this.species;
+	}
+	
+	public double getPurchasePrice() {
+		return this.purchasePrice;
+	}
+	
+	public void setPurchasePrice(double purchasePrice) {
+		this.purchasePrice = purchasePrice;
+	}
+	
+	public double getSellingPrice() {
+		return this.sellingPrice;
+	}
+	
+	public void setSellingPrice(double sellingPrice) {
+		this.sellingPrice = sellingPrice;
+	}
+	
+	public int getDaysUntilMature() {
+		return this.daysUntilMature;
+	}
+	
+	public void setDaysUntilMature(int days, int growthBonus) {
+		this.daysUntilMature = days - growthBonus;
+		if(daysUntilMature < 0) daysUntilMature = 0;
+	}
+	
+	public double getHealth() {
+		return this.health;
+	}
+	
+	public void setHealth(double health) {
+		this.health = health;
+	}
+	//END getters and setters
+	
+	//BEGIN methods
+	public void reduceDaysUntilMature(int reduction) {
+		daysUntilMature -= reduction;
+		if(daysUntilMature < 0) daysUntilMature = 0;
+	}
+	//END methods
+}
---
+++ SENG-201-Project/src/main/CropItem.java
@@ -0,0 +1,19 @@
+package main;
+
+public class CropItem extends Item {
+	private int growthEnhancement;
+	
+	public CropItem(String name, double price, int growthEnhancement) {
+		super(name, price);
+		this.growthEnhancement = growthEnhancement;
+	}
+	
+	//BEGIN getters and setters
+	public int getGrowthEnhancement() {
+		return this.growthEnhancement;
+	}
+	
+	public void setGrowthEnhancement(int growthEnhancement) {
+		this.growthEnhancement = growthEnhancement;
+	}
+}
